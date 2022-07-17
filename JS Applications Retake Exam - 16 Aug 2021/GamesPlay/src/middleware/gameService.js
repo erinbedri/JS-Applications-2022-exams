087dfcf9fb6fff26320e295aa03d19b27ed9c562
erinbedri <erinbedri@gmail.com>
2022-07-17 15:46:47 +0200

    home page

--- JS Applications Retake Exam - 16 Aug 2021/GamesPlay/src/middleware/gameService.js
+++ JS Applications Retake Exam - 16 Aug 2021/GamesPlay/src/middleware/gameService.js
@@ -3,3 +3,5 @@ import * as request from './requester.js'
 const baseUrl = 'http://localhost:3030';
 
 export const getAll = () => request.get(`${baseUrl}/data/games?sortBy=_createdOn%20desc`);
+
+export const getAllForHomePage = () => request.get(`${baseUrl}/data/games?sortBy=_createdOn%20desc&distinct=category`);
--- JS Applications Retake Exam - 16 Aug 2021/GamesPlay/src/views/homeView.js
+++ JS Applications Retake Exam - 16 Aug 2021/GamesPlay/src/views/homeView.js
@@ -1,3 +1,47 @@
+import { html } from '../../node_modules/lit-html/lit-html.js'
+
+import * as gameService from '../middleware/gameService.js';
+
+const gameTemplate = (game) => html`
+                <div class="game">
+                    <div class="image-wrap">
+                        <img src=${game.imageUrl}>
+                    </div>
+                    <h3>${game.title}</h3>
+                    <div class="rating">
+                        <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+                    </div>
+                    <div class="data-buttons">
+                        <a href="/details/${game._id}" class="btn details-btn">Details</a>
+                    </div>
+                </div>
+`;
+
+const homeTemplate = (games) => html`
+        <section id="welcome-world">
+        
+            <div class="welcome-message">
+                <h2>ALL new games are</h2>
+                <h3>Only in GamesPlay</h3>
+            </div>
+            <img src="/images/four_slider_img01.png" alt="hero">
+        
+            <div id="home-page">
+                <h1>Latest Games</h1>
+
+                ${games
+                    ? html`
+                        ${games.map(game => gameTemplate(game))}
+                    `
+                    : html`
+                        <p class="no-articles">No games yet</p>
+                    `
+            }
+`;
+
 export const homeView = (ctx) => {
-    console.log('home');
+    gameService.getAllForHomePage()
+        .then(games => {
+            ctx.render(homeTemplate(games));
+        })
 }
