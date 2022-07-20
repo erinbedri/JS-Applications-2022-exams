881eae6b32e13beb2d2caf38e1262ea5c8eb0c85
erinbedri <erinbedri@gmail.com>
2022-07-20 14:06:19 +0200

    login template

--- JS Applications Exam - 2 April 2022/Pet Care/src/views/loginView.js
+++ JS Applications Exam - 2 April 2022/Pet Care/src/views/loginView.js
@@ -1,5 +1,30 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
 
+const loginTemplate = () => html`
+        <section id="loginPage">
+            <form class="loginForm">
+                <img src="./images/logo.png" alt="logo" />
+                <h2>Login</h2>
+
+                <div>
+                    <label for="email">Email:</label>
+                    <input id="email" name="email" type="text" placeholder="steven@abv.bg" value="">
+                </div>
+
+                <div>
+                    <label for="password">Password:</label>
+                    <input id="password" name="password" type="password" placeholder="********" value="">
+                </div>
+
+                <button class="btn" type="submit">Login</button>
+
+                <p class="field">
+                    <span>If you don't have profile click <a href="/register">here</a></span>
+                </p>
+            </form>
+        </section>
+`;
+
 export const loginView = (ctx) => {
-    console.log('login');
+    ctx.render(loginTemplate());
 }
