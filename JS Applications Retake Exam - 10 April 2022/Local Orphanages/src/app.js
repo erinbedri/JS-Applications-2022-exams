0372b83fd9fe82ad289c1e39ca47e0a77b0baa81
erinbedri <erinbedri@gmail.com>
2022-07-26 12:07:17 +0200

    initial setup

---
+++ JS Applications Retake Exam - 10 April 2022/Local Orphanages/src/app.js
@@ -0,0 +1,36 @@
+import page from '../node_modules/page/page.mjs';
+
+import { renderNavigationMiddleware, renderContentMiddleware } from './middleware/renderMiddleware.js';
+import { authMiddleware } from './middleware/authMiddleware.js';
+
+/*
+import { homeView } from './views/homeView.js';
+import { dashboardView } from './views/dashboardView.js';
+import { loginView } from './views/loginView.js';
+import { registerView } from './views/registerView.js';
+import { logoutView } from './views/logoutView.js';
+import { createView } from './views/createView.js';
+import { detailsView } from './views/detailsView.js';
+import { deleteView } from './views/deleteView.js';
+import { editView } from './views/editView.js';
+import { donateView } from './views/donateView.js';
+*/
+
+page(authMiddleware);
+page(renderNavigationMiddleware);
+page(renderContentMiddleware);
+
+/*
+page('/', homeView);
+page('/dashboard', dashboardView);
+page('/login', loginView);
+page('/register', registerView);
+page('/logout', logoutView);
+page('/create', createView);
+page('/details/:id', detailsView);
+page('/delete/:id', deleteView);
+page('/edit/:id', editView);
+page('/donate/:id', donateView)
+*/
+
+page.start()
---
+++ JS Applications Retake Exam - 10 April 2022/Local Orphanages/src/middleware/postService.js
@@ -0,0 +1,15 @@
+import * as request from './requester.js'
+
+const baseUrl = 'http://localhost:3030';
+
+/*
+export const getAll = () => request.get(`${baseUrl}/data/pets?sortBy=_createdOn%20desc&distinct=name`);
+
+export const getOne = (petId) => request.get(`${baseUrl}/data/pets/${petId}`);
+
+export const edit = (editedPet, petId) => request.put(`${baseUrl}/data/pets/${petId}`, editedPet);
+
+export const delPet = (petId) => request.del(`${baseUrl}/data/pets/${petId}`); 
+
+export const createPet = (newPet) => request.post(`${baseUrl}/data/pets`, newPet);
+*/
